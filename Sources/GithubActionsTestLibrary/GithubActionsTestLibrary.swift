e4310713ece941fb76e1ffffda94f7132b53817f
vebbis321 <veb.dan@hotmail.com>
2023-08-17 21:31:40 +0200

    Fixed violations

--- Sources/GithubActionsTestLibrary/GithubActionsTestLibrary.swift
+++ Sources/GithubActionsTestLibrary/GithubActionsTestLibrary.swift
@@ -1,6 +1,5 @@
 public struct GithubActionsTestLibrary {
-    public private(set) var text = "Hello, World!"
+  public private(set) var text = "Hello, World!"
 
-    public init() {
-    }
+  public init() {}
 }
